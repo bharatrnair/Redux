7cbb876228fb72f851c07f51b3f1b75fa4e65b26
bharathrnair <bharathrnair7@gmail.com>
2022-04-13 09:59:59 +0530

    commit

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const redux = require("redux");
-const {createStore} = redux;
+const {createStore, combineReducers} = redux;
 
-const reducer =(state =[],action)=>{
+const todo =(state =[],action)=>{
     switch (action.type){
         case "ADD_TODO":
             return [...state,action.payload];
+        case "REMOVE_TODO":
+            return state.filter((todo)=>todo !==action.payload);
         default:
             return state;
     }
 };
 
+const counter =(state = 0,action)=>{
+    switch (action.type){
+        case "INCREMENT":
+            return state + 1;
+        case "DECREMENT":
+            return state - 1;
+        default:
+            return state;
+    }
+};
+
+const reducer = combineReducers({
+    todo,
+    counter,
+});
 const store = createStore(reducer);
+
 
 store.subscribe(()=>{
     console.log(store.getState());
@@ -24,4 +42,17 @@ store.dispatch({
 store.dispatch({
     type:"ADD_TODO",
     payload:"Learn React"
+});
+
+store.dispatch({
+    type:"INCREMENT"
+})
+
+store.dispatch({
+    type:"REMOVE_TODO",
+    payload:"Learn Redux"
+});
+
+store.dispatch({
+    type:"DECREMENT"
 });
